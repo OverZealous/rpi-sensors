2952bda990fbd93ba3f95556bde010021ae36a3c
Phil DeJarnett <phil@overzealous.com>
2016-10-13 21:54:04 -0400

    Added Joystick via pcf8591

--- joystick.js
+++ joystick.js
@@ -1,36 +1,70 @@
-var wpi = require('wiring-pi');
+/**
+ * Reads input from the joystick module via the pcf8591 module
+ */
+const wpi = require('wiring-pi');
+const pcf8591 = require('./lib/pcf8591');
 
-wpi.setup('phys');
-const btnPin = 11;
-const yAxisPin = 12;
-const xAxisPin = 13;
+const Y_AXIS = 0;
+const X_AXIS = 1;
+const HOME = 128;
+const CENTER_BUFFER = 20;
+const BTN = 2;
 
-wpi.pinMode(btnPin, wpi.INPUT);
-wpi.pullUpDnControl(btnPin, wpi.PUD_DOWN);
-wpi.pinMode(yAxisPin, wpi.INPUT);
-wpi.pullUpDnControl(btnPin, wpi.PUD_UP);
-wpi.pinMode(xAxisPin, wpi.INPUT);
-wpi.pullUpDnControl(btnPin, wpi.PUD_UP);
+wpi.setup('wpi');
 
-function register(pin) {
-	wpi.wiringPiISR(pin, wpi.INT_EDGE_BOTH, function(delta) {
-		console.log(pin, pin === btnPin ? wpi.digitalRead(pin) : wpi.analogRead(pin), delta);
-		register(pin);
-	});
+const pcf = pcf8591.init();
+
+const DIRECTIONS = {
+	'Top': '↑',
+	'Bottom': '↓',
+	'Right': '→',
+	'Left': '←',
+	'Top Right': '︎↗︎',
+	'Top Left': '↖',
+	'Bottom Right': '↘',
+	'Bottom Left': '↙︎',
+	'Center': '·',
+};
+
+function read() {
+	let vertical, horizontal;
+	let yVal = pcf.read(Y_AXIS);
+	let xVal = pcf.read(X_AXIS);
+	let btnPressed = pcf.read(BTN) < 100;
+	if(yVal < HOME - CENTER_BUFFER) vertical = 'Top';
+	if(yVal > HOME + CENTER_BUFFER) vertical = 'Bottom';
+	if(xVal < HOME - CENTER_BUFFER) horizontal = 'Right';
+	if(xVal > HOME + CENTER_BUFFER) horizontal = 'Left';
+
+	let state;
+	if(vertical) {
+		state = vertical + (horizontal ? ' ' + horizontal : '');
+	} else if(horizontal) {
+		state = horizontal;
+	} else {
+		state = 'Center';
+	}
+
+	state = `[${DIRECTIONS[state]}](${btnPressed ? '•' : ' '}) ${state}, Button ${(btnPressed ? 'Down' : 'Up')}`;
+	state = rpad(state, 40);
+
+	process.stdout.write("\r" + state);
+	setTimeout(read, 10);
 }
-register(btnPin);
-register(yAxisPin);
-register(xAxisPin);
 
+function rpad(s, len) {
+	s = '' + s;
+	while(s.length < len) s += ' ';
+	return s;
+}
 
-var exited = false;
+read();
+
+let exited = false;
 function exit() {
 	if(exited) return;
 	exited = true;
-	console.log('\rExiting');
-	wpi.wiringPiISRCancel(btnPin);
-	wpi.wiringPiISRCancel(yAxisPin);
-	wpi.wiringPiISRCancel(xAxisPin);
+	console.log('\nExiting');
 	process.exit(0);
 }
 
